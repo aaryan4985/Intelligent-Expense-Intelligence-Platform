0a90aa783984cc06cfd90eb4a2bffe8c17557dee
Aaryan Pradhan <pradhanaaryan@gmail.com>
2026-03-10 12:35:30 +0530

    Initital

---
+++ src/main/java/com/aaryan/expense_intelligence/ExpenseIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.aaryan.expense_intelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseIntelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaryan/expense_intelligence/ExpenseIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaryan.expense_intelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
