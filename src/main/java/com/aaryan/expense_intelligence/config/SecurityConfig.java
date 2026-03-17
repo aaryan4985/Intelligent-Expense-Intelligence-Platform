b24680e8f787411220ea0d43f5f41cc5a657c40b
Aaryan Pradhan <pradhanaaryan@gmail.com>
2026-03-17 12:40:10 +0530

    filter

--- src/main/java/com/aaryan/expense_intelligence/config/SecurityConfig.java
+++ src/main/java/com/aaryan/expense_intelligence/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.aaryan.expense_intelligence.config;
 
+import com.aaryan.expense_intelligence.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,16 +9,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().permitAll());
+                        .requestMatchers("/auth/**", "/h2-console/**").permitAll()
+                        .anyRequest().authenticated());
 
         http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+
+        http.addFilterBefore(jwtFilter,
+                org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
